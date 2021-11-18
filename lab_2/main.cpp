7c1ddfb0a8b9c72cba9cd80f61c0a5c943872f28
Evgeny Kuryatov <evgeny.kuryatov@netcracker.com>
2021-11-18 17:33:22 +0300

    Initial Commit

---
+++ lab_2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lab_2
+//
+//  Created by Евгений Курятов on 18.11.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
