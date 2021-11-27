af8c5ebe43f8e8b9fd73a00e3375ff60a71a3acf
Evgeny Kuryatov <evgeny.kuryatov@netcracker.com>
2021-11-27 23:09:13 +0300

    Add Iterator

--- lab_2/array.hpp
+++ lab_2/array.hpp
@@ -23,6 +23,7 @@ private:
     int capacity_;
 private:
     void extend();
+    
 public:
     Array();
     Array(int capacity);
@@ -32,8 +33,14 @@ public:
     private:
         int current_index_;
         T& current_value_;
+        int type_;
     public:
-        Iterator(Array *a);
+        const static int kReverceType = -1;
+        const static int kNotReverceType = 1;
+    private:
+        Array* arr_;
+    public:
+        Iterator(Array *a, int type);
         T& get() const;
         void set(const T& value);
         void next();
@@ -61,16 +68,59 @@ public:
 //    ConstIterator reverseIterator() const;
 };
 
+
+// ====================================================
+//                Iterator definitions
+// ====================================================
+
 template <typename T>
-Array<T>::Iterator::Iterator(Array *a) {
-    current_index_ = 0;
-    current_value_ = a->at(current_index_);
+Array<T>::Iterator::Iterator(Array *a, int type)
+    : current_value_(a->at(0))
+    , type_(type)
+    , arr_(a) {
+        if (type_ == Iterator::kReverceType)
+            current_index_ = a->size() - 1;
+        else
+            current_index_ = 0;
 }
 
 template <typename T>
 T& Array<T>::Iterator::get() const {
-    return Array.this.at(current_index_);
+    return arr_->at(current_index_);
 }
+
+template <typename T>
+void Array<T>::Iterator::set(const T& value) {
+    arr_[current_index_] = value;
+}
+
+template <typename T>
+void Array<T>::Iterator::next() {
+    current_index_ += type_;
+}
+
+template <typename T>
+bool Array<T>::Iterator::hasNext() const {
+    if (type_ == Iterator::kNotReverceType)
+        return !(current_index_ == arr_->size() - 1);
+    else
+        return !(current_index_ == 0);
+}
+
+template <typename T>
+typename Array<T>::Iterator Array<T>::iterator(){
+    Iterator it(this, Array<T>::Iterator::kNotReverceType);
+    return it;
+}
+
+template <typename T>
+typename Array<T>::Iterator Array<T>::reverseIterator(){
+    Iterator it(this, Array<T>::Iterator::kReverceType);
+    return it;
+}
+// ====================================================
+//                  Array defenitions
+// ====================================================
 
 template <typename T>
 void Array<T>::extend() {
@@ -80,14 +130,14 @@ void Array<T>::extend() {
     for (int i = 0; i < count_; i++){
         T& current_value = at(i);
         if (std::is_move_constructible<T>::value) {
-            new_buffer[i] = std::move_if_noexcept(current_value);
+            new(new_buffer + i) T(std::move_if_noexcept(current_value));
         } else {
-            new_buffer[i] = current_value;
+            new(new_buffer + i) T(current_value);
         }
     }
+    for (auto it = this->iterator(); it.hasNext(); it.next()) it.get().~T();
     free(buffer_);
     buffer_ = new_buffer;
-//    delete [] buffer_;
 }
 
 template <typename T>
@@ -106,7 +156,7 @@ Array<T>::Array(int capcity) {
 
 template <typename T>
 Array<T>::~Array() {
-//    delete [] buffer_;
+    for (auto it = this->iterator(); it.hasNext(); it.next()) it.get().~T();
     free(buffer_);
 }
 
@@ -115,7 +165,11 @@ int Array<T>::insert(const T& value) {
     if (count_ >= capacity_) {
         extend();
     }
-    *(buffer_ + count_) = value;
+    if (std::is_move_constructible<T>::value) {
+        new(buffer_ + count_) T(std::move_if_noexcept(value));
+    } else {
+        new(buffer_ + count_) T(value);
+    }
     count_ += 1;
     return count_;
 }
--- lab_2/main.cpp
+++ lab_2/main.cpp
@@ -8,12 +8,49 @@
 #include <iostream>
 #include "array.hpp"
 
+class TestClass {
+public:
+    int value_;
+    bool debug_;
+    const int b = 3;
+public:
+    TestClass(int val, bool debug)
+        : value_(val)
+        , debug_(debug) {
+        value_ = val;
+        if (debug_) std::cout << "Constructed [" << value_ << "]\n";
+    }
+    ~TestClass(){
+        if (debug_) std::cout << "Destructed [" << value_ << "]\n";
+    }
+};
+
+std::ostream& operator<<(std::ostream& os, const TestClass& te)
+{
+    os << '[' << te.value_ << ']';
+    return os;
+}
+
 int main() {
-    Array<int> a;
-    for (int i = 0; i < 10; ++i)
-        a.insert(i + 1);
+    Array<TestClass> a;
+    for (int i = 0; i < 10; ++i) {
+        TestClass te(i, false);
+        a.insert(te);
+    }
     for (int i = 0; i < a.size(); ++i)
-        a[i] *= 2;
+        a[i].value_ *= 2;
     a.printArrayInfo();
+    for (auto it = a.iterator(); it.hasNext(); it.next())
+      std::cout << it.get() << std::endl;
+    
+    Array<int> b;
+    for (int i = 0; i < 10; ++i) {
+        b.insert(i);
+    }
+    for (int i = 0; i < b.size(); ++i)
+        b[i] *= 2;
+    b.printArrayInfo();
+    for (auto it = b.reverseIterator(); it.hasNext(); it.next())
+      std::cout << it.get() << std::endl;
     return 0;
 }
